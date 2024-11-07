647238abe73738b6eb1cec1237798f7d845cad43
FillDDak <cgr456@naver.com>
2024-11-07 11:40:55 +0900

    학교에서 한거 (App bar 수정 필요)

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,9 +1,24 @@
 <template>
   <v-app>
     <v-main>
+      <v-app-bar :elevation="0" app dark :height="90" scroll-behavior="hide" scroll-threshold="200">
+      <v-app-bar-title>
+        <v-img src="./assets/readylogo.png" alt="Logo" contain max-width="250" class="mr-2" @click="$router.push('/')"
+          style="cursor: pointer;"></v-img>
+        </v-app-bar-title>
+        <v-spacer></v-spacer>
+     
+        <v-btn class="mr-2">Home</v-btn>
+        <v-btn class="mr-2">About</v-btn>
+        <v-btn class="mr-2">Services</v-btn>
+        <v-btn class="mr-2">Contact</v-btn>
+        <v-btn style="background-color: #BADC6B; font-size: 16px;" class="mr-2 font-weight-bold" rounded="xl" @click="loginDialog = true">로그인</v-btn>
+        <v-btn style="background-color: lightgray; font-size: 16px;" class="mr-3 font-weight-bold" rounded="xl" @click="signupDialog = true">회원가입</v-btn>
+      </v-app-bar>
       <router-view/>
     </v-main>
   </v-app>
+  
 </template>
 
 <script>
--- front/src/views/MainView.vue
+++ front/src/views/MainView.vue
@@ -1,21 +1,5 @@
 <template>
   <v-app>
-    <!-- 앱 바 -->
-    <v-app-bar :elevation="0" app dark :height="90">
-      <v-app-bar-title>
-        <v-img src="../assets/readylogo.png" alt="Logo" contain max-width="250" class="mr-2" @click="$router.push('/')"
-          style="cursor: pointer;"></v-img>
-      </v-app-bar-title>
-      <v-spacer></v-spacer>
-     
-      <v-btn class="mr-2">Home</v-btn>
-      <v-btn class="mr-2">About</v-btn>
-      <v-btn class="mr-2">Services</v-btn>
-      <v-btn class="mr-2">Contact</v-btn>
-      <v-btn style="background-color: #BADC6B; font-size: 16px;" class="mr-2" rounded="xl" @click="loginDialog = true">로그인</v-btn>
-      <v-btn style="background-color: lightgray; font-size: 16px;" class="mr-3" rounded="xl" @click="signupDialog = true">회원가입</v-btn>
-    </v-app-bar>
-
     <!-- 로그인 다이얼로그 -->
     <v-dialog v-model="loginDialog" max-width="400">
       <v-card>
