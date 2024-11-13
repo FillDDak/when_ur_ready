5effad5ffa599c1bd8da75e0cce630b60f862c88
FillDDak <cgr456@naver.com>
2024-11-14 02:08:59 +0900

    로그인/회원가입/로그인 상태 유지 기능 추가
    로그인/회원가입 검증 구체화 필요

--- backend/app.js
+++ backend/app.js
@@ -10,13 +10,32 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // sequelize 모듈 가져오기, 데이터베이스 연결
-var { Sequelize } = require('sequelize')
-var sequelize = new Sequelize("when_ur_ready", "root", "!Aa5520634", {
+var { Sequelize } = require('sequelize');
+
+// 초기 연결 (데이터베이스 없이)
+var sequelize = new Sequelize("", "root", "123456", {
   host: "localhost",
-  dialect: "mysql" // 데이터베이스에 맞는 sql문 연결
-})
-global.sequelize = sequelize; // 전역 변수 선언
-require("./model")(Sequelize, sequelize) // 모델 불러오기 및 데이터베이스 모델 정의
+  dialect: "mysql" // 데이터베이스에 맞는 SQL문 연결
+});
+
+// MySQL2 root 비밀번호 변경 방법 (비번: 123456)
+// MySQL2 Workbench에서 쿼리 생성 > 아래 코드 입력 > ctrl + enter
+// ALTER USER 'root'@'localhost' IDENTIFIED BY '123456';
+
+// 스키마가 없으면 생성
+sequelize.query("CREATE DATABASE IF NOT EXISTS `when_ur_ready` CHARACTER SET utf8mb4;")
+  .then(() => {
+    // 데이터베이스 사용 설정 (재연결)
+    sequelize = new Sequelize("when_ur_ready", "root", "123456", {
+      host: "localhost",
+      dialect: "mysql"
+    });
+    global.sequelize = sequelize; // 전역 변수 선언
+    require("./model")(Sequelize, sequelize); // 모델 불러오기 및 데이터베이스 모델 정의
+  })
+  .catch(err => {
+    console.error('데이터베이스 생성 오류:', err);
+  });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,6 +45,26 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// 사용자 로그인 세션을 MySQL에 저장하여 세션 데이터를 영구적으로 유지
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
+const options = {
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: '123456',
+  database: 'when_ur_ready'
+};
+const sessionStore = new MySQLStore(options);
+app.use(session({
+  key: 'session_id',
+  secret: '!@#QWEASDZXC',
+  store: sessionStore,
+  resave: false,
+  saveUninitialized: false
+}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
